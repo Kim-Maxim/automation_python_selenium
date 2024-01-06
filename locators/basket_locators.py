7da130eb6641fb1e93a5932638c902c09ea0d1a5
Maxim Kim <kimmaxim69@gmail.com>
2024-01-06 20:02:18 +0300

    Переделал локаторы сделал их более уникальными

--- locators/basket_locators.py
+++ locators/basket_locators.py
@@ -3,4 +3,4 @@ from selenium.webdriver.common.by import By
 
 class BasketPageLocators:
     # Локаторы для страницы корзины
-    NAME_PRODUCT = (By.XPATH, "//a[contains(text(), 'Redmi Note')]")
+    NAME_PRODUCT = (By.XPATH, "//h3/a")
